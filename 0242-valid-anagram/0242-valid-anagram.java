4c029e2f16017324cbe4efc3f95b3429ae294ad0
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-13 19:48:18 +0530

    Time: 2 ms (97.64%), Space: 43.6 MB (58.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
        int[] a1=new int[26];
-       int[] a2=new int[26];
+      // int[] a2=new int[26];
        for(char ch:s.toCharArray()){
         a1[ch-'a']++;
        }
        for(char ch:t.toCharArray()){
-        a2[ch-'a']++;
+        a1[ch-'a']--;
        }
        for(int i=0;i<26;i++){
-        if(a1[i]!=a2[i]) return false;
+        if(a1[i]!=0) return false;
        }
        return true;
     }
