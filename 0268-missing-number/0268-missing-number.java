37a06b328ada74c02ec1de1b735e711c7556a616
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-15 18:20:14 +0530

    Time: 5 ms (23.74%), Space: 45.7 MB (22.47%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        map.put(nums[i],0);
+      }
+      for(int i=0;i<=nums.length;i++){
+        if(!map.containsKey(i)) return i;
+      }
+      return 0;
+    }
+}
