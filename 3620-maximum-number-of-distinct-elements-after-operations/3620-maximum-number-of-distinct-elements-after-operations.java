ac2dbb522b8e0cdf1edfd1c7a348d29ce18ce089
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-20 10:40:09 +0530

    Time: 19 ms (81.9%), Space: 58.5 MB (18.56%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.java
@@ -4,13 +4,15 @@ class Solution {
         int n=nums.length;
         int count=0;
         int prev=Integer.MIN_VALUE;
-        int temp=nums[n-1]+k;
         for(int i=0;i<n;i++){
-           int l=Math.max(nums[i]-k,prev+1);
-           if(l<=nums[i]+k){
-            prev=l;
-            count++;
-           }
+            int min=nums[i]-k;
+            if(prev<min){
+                prev=min;
+                count++;
+            }else if(prev<nums[i]+k){
+                prev=prev+1;
+                count++;
+            }
         }
         return count;
     }
