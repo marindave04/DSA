707a051cbae386c5ef1e4680fa1de76104fbd4ab
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-21 19:40:58 +0530

    Time: 6 ms (96.71%), Space: 61.5 MB (54.6%) - LeetHub

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(max,nums[i]);
+        }
+        int[] cum=new int[max+k+1];
+        for(int i=0;i<nums.length;i++){
+             cum[nums[i]]++;
+        }
+        for(int i=1;i<cum.length;i++){
+            cum[i]=cum[i]+cum[i-1];
+        }
+        int temp=0;
+        for(int target=0;target<=max;target++){
+            if(cum[target]==0) continue;
+            int l=Math.max(0,target-k);
+            int r=Math.min(target+k,cum.length-1);
+            int totalcount=cum[r]-(l>0 ?cum[l-1] : 0);
+            int targetcount=cum[target]-(target>0 ? cum[target-1]:0);
+            int reqop=totalcount-targetcount;
+            int freq=targetcount+Math.min(reqop,numOperations);
+            temp=Math.max(temp,freq);
+        }
+        return temp;
+    }
+}
