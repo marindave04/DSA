8992a61de0433e0ec7c4854c073aaa7ef242412d
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-22 11:05:27 +0530

    Time: 62 ms (70.86%), Space: 62.5 MB (53.64%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,27 +1,27 @@
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
         int max=0;
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             max=Math.max(max,nums[i]);
         }
-        int[] cum=new int[max+k+1];
-        for(int i=0;i<nums.length;i++){
-             cum[nums[i]]++;
-        }
-        for(int i=1;i<cum.length;i++){
-            cum[i]=cum[i]+cum[i-1];
-        }
-        int temp=0;
-        for(int target=0;target<=max;target++){
-            //if(cum[target]==0) continue;
-            int l=Math.max(0,target-k);
-            int r=Math.min(target+k,cum.length-1);
-            int totalcount=cum[r]-(l>0 ?cum[l-1] : 0);
-            int targetcount=cum[target]-(target>0 ? cum[target-1]:0);
-            int reqop=totalcount-targetcount;
-            int freq=targetcount+Math.min(reqop,numOperations);
-            temp=Math.max(temp,freq);
-        }
-        return temp;
+       int[] diff=new int[max+k+2];
+       for(int i=0;i<n;i++){
+        int l=Math.max(0,nums[i]-k);
+        int r=Math.min(nums[i]+k,diff.length-1);
+        diff[l]++;
+        diff[r+1]--;
+       }
+       int ans=1;
+       for(int target=0; target<=max+k;target++ ){
+           diff[target] +=(target>0 ? diff[target-1] : 0);
+           int targetFreq = map.getOrDefault(target, 0);
+           int reqop=diff[target]-targetFreq;
+           int totalFreq=Math.min(reqop,numOperations)+targetFreq;
+           ans=Math.max(ans,totalFreq);
+       }
+       return ans;
     }
 }
