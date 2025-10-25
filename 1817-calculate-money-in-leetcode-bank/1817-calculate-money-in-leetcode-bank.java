1e598424f51e24cc37dfbbf674e41b158f23b9e5
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-25 14:43:52 +0530

    Time: 0 ms (100%), Space: 40.4 MB (58.3%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,15 +1,17 @@
 class Solution {
     public int totalMoney(int n) {
        if(n<=7) return n*(n+1)/2;
-       int a=28;
-       int d=7;
-       int week=n/7;
+       int a1=28;
+       int d1=7;
+       int n1=n/7;
        int days=n%7;
-       int l=a+(week-1)*d;
-       double s1=(week/2.0)*(a+l);
-       for(int i=week+1;i<=week+days;i++ ){
-        s1=s1+i;
-       }
-       return (int)s1;
+       int l1=a1+(n1-1)*d1;
+       double s1=(n1/2.0)*(a1+l1);
+       int a2=n1+1;
+       int d2=1;
+       int n2=days;
+       int l2=a2+(n2-1)*d2;
+       double s2=(n2/2.0)*(a2+l2);
+       return (int)(s1+s2);
     }
 }
