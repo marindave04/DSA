b7f9d6204166a019416f3c5200ae85af410f473a
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-27 13:25:19 +0530

    Time: 1 ms (79.87%), Space: 43.4 MB (34.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -6,29 +6,29 @@ class Solution {
     }
     static void generate(int n,String current, ArrayList<String> list,int open,int close){
         if(current.length()==2*n){
-            if(Valid(current)){
-                list.add(current);
-                return;
-            }else{
-                return;
-            }
-            // list.add(current);
-            // return;
+            // if(Valid(current)){
+            //     list.add(current);
+            //     return;
+            // }else{
+            //     return;
+            // }
+            list.add(current);
+            return;
         }
         if(open<n)generate(n,current+"(",list,open+1,close);
-        generate(n,current+")",list,open,close+1);
+        if(close<open)generate(n,current+")",list,open,close+1);
     }
-     static boolean Valid(String str) {
-        int sum = 0;
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(ch=='('){
-                sum +=1;
-            }else{
-                sum -=1;
-                if(sum<0) return false;
-            }
-        }
-        return sum == 0;
-    }
+    //  static boolean Valid(String str) {
+    //     int sum = 0;
+    //     for(int i=0;i<str.length();i++){
+    //         char ch=str.charAt(i);
+    //         if(ch=='('){
+    //             sum +=1;
+    //         }else{
+    //             sum -=1;
+    //             if(sum<0) return false;
+    //         }
+    //     }
+    //     return sum == 0;
+    // }
     }
