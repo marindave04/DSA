06f834f7fce6257011591835b91b22822bc560eb
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-27 09:35:22 +0530

    Time: 10 ms (89.6%), Space: 45.2 MB (59.97%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -2,22 +2,18 @@ class Solution {
     public int numberOfBeams(String[] bank) {
         int n=bank.length;
         int count=0;
-        int temp=0;
-        int prev=0;
-        for(int i=0;i<n-1;i++){
-            int cr=ones(bank[i]);
-            for(int j=i+1;j<n;j++){
-                int nr=ones(bank[j]);
-                if(j>i+1 && prev>0){
-                    continue;
-                }else{
-                    temp=cr*nr;
-                    count=count+temp;
-                    prev=nr;
-                }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int temp=ones(bank[i]);
+            if(temp!=0){
+                list.add(temp);
             }
         }
-       return count;
+        for(int i=0;i<list.size()-1;i++){
+            int product=list.get(i)*list.get(i+1);
+            count=count+product;
+        }
+        return count;
     }
     static int ones(String a){
         int count=0;
