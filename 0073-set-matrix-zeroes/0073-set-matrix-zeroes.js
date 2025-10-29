c9cf0096ee8b4b37b1a003d30c15a220c83d790a
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-10-29 23:33:22 +0530

    Time: 3 ms (67.84%), Space: 59.5 MB (53.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -5,30 +5,45 @@
 var setZeroes = function(matrix) {
     let m=matrix.length;
     let n=matrix[0].length;
-    let map1=new Map();
-    let map2=new Map();
+    //check if first row is impacted
+        let firstRowImpacted=false;
+        for(let col=0;col<n;col++){
+            if(matrix[0][col]==0){
+            firstRowImpacted=true;
+            }
+        }
+        //check if first col is impacted
+        let firstColImpacted=false;
+        for(let row=0;row<m;row++){
+            if(matrix[row][0]==0){
+            firstColImpacted=true;
+            }
+        }
+         //marking two flags which will helps us whic rows and columns are impacted
     for(let i=0;i<m;i++){
         for(let j=0;j<n;j++){
             if(matrix[i][j]==0){
-                map1.set(i,matrix[i][j]);
-                map2.set(j,matrix[i][j]);
+               matrix[i][0]=0;
+               matrix[0][j]=0;
             }
         }
     }
-    for(let [key,value] of map1.entries()){
-        if(value==0){
-            let row=key;
-            for (let p = 0; p < n; p++) {
-                matrix[row][p] = 0;
+     for(let i=1;i<m;i++){
+            for(let j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
-    }
-    for(let [key,value] of map2.entries()){
-        if(value==0){
-            let col=key;
-            for (let q = 0; q < m; q++) {
-                matrix[q][col] = 0;
-            }
+        if(firstRowImpacted){
+            for(let col=0;col<n;col++){
+           matrix[0][col]=0;
         }
-    }
+        }
+        if(firstColImpacted){
+            for(let row=0;row<m;row++){
+            matrix[row][0]=0;
+        }
+        }
+    
 };
