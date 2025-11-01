397da605ea713dd4bff53ab44d61243754dbc478
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-11-01 14:11:33 +0530

    Time: 1 ms (100%), Space: 45.5 MB (7.79%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,31 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int[] ans= new int[2];
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
         int n=nums.length-2;
-        int index=0;
-        for(int i=0;i<=n;i++){
-            if(map.containsKey(i) && map.get(i)==2){
-               ans[index++]=i;
+        
+        int xorOfTwoNumber=0;
+        for(int i=0;i<nums.length;i++){
+            xorOfTwoNumber^=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            xorOfTwoNumber^=i;
+        }
+        int firstBitDiff=xorOfTwoNumber & -xorOfTwoNumber;
+        int g1=0;
+        int g2=0;
+        for(int i=0;i<nums.length;i++){
+            if((firstBitDiff & nums[i])==0){
+                g1^=nums[i];
+            }else{
+                g2^=nums[i];
             }
         }
-        return ans;
+        for(int i=0;i<n;i++){
+            if((firstBitDiff & i)==0){
+                g1^=i;
+            }else{
+                g2^=i;
+            }
+        }
+        return new int[] {g1,g2};
     }
 }
