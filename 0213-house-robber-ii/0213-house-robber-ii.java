731ab90b2db619e462de58985643ab60755506fd
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-11-22 07:08:38 +0530

    Time: 1 ms (5.02%), Space: 43 MB (11.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -10,11 +10,17 @@ class Solution {
         int b=solve(nums,1,n-1,dp2);
         return (int)Math.max(a,b);
     }
-    static int solve(int[] nums,int s,int e,int[]dp){
-        if(s>e) return 0;
-        if(dp[s]!=-1) return dp[s];
-        int notSkip=nums[s]+solve(nums,s+2,e,dp);
-        int skip=solve(nums,s+1,e,dp);
-        return dp[s]=(int)Math.max(notSkip,skip);
+    int solve(int[] nums,int s,int e,int[]dp){
+       int len=(e-s)+1;
+       dp[0]=0;
+       dp[1]=nums[s];
+       int idx=2;
+       for(int i=idx;i<=len;i++){
+       int pick=nums[idx-1+s]+dp[idx-2];
+       int notPick=dp[idx-1];
+       dp[idx]=(int) Math.max(pick,notPick);
+       idx++;
+       }
+      return dp[len]; 
     }
 }
