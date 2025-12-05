c2084f97eb443754f5b34aa31a8aafad5d91a91f
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-12-05 12:26:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.java
@@ -1,14 +1,12 @@
 class Solution {
     public int towerOfHanoi(int n, int from, int to, int aux) {
         // code here
-        return solve(n,from,to,aux);
-    }
-    static int solve(int n, int from, int to, int aux){
+        return solve(n,0,0,0);
+    }  
+    static int solve(int n,int from,int to,int aux){
         if(n==0) return 0;
         if(n==1) return 1;
-        
-        int x= solve(n-1,from,aux,to);
-       // count++;
+        int x=solve(n-1,from,aux,to);
         int y=solve(n-1,aux,to,from);
         return x+y+1;
     }
