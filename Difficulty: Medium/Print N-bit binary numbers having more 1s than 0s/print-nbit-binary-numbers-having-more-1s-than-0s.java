75cc885fb2f4cbb5d38f28396490d443527cbe33
marindave04 <mdsadainsiddiqui973@gmail.com>
2025-12-06 09:58:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print N-bit binary numbers having more 1s than 0s/print-nbit-binary-numbers-having-more-1s-than-0s.java
+++ Difficulty: Medium/Print N-bit binary numbers having more 1s than 0s/print-nbit-binary-numbers-having-more-1s-than-0s.java
@@ -1,38 +1,23 @@
 // User function Template for Java
 
 class Solution {
+    private ArrayList<String> list=new ArrayList<>();
     ArrayList<String> NBitBinary(int N) {
         // code here
-         ArrayList<String> list=new ArrayList<>();
-         solve("1",N,list);
-         Collections.sort(list, Collections.reverseOrder());
-         return list;
+        solve(N,0,1,"1");
+        Collections.sort(list, Collections.reverseOrder());
+        return list;
     }
-    static void solve(String s,int n,ArrayList<String> list){
+    private void solve(int n,int count0,int count1,String s){
         if(s.length()==n){
-            if(check(s)){
-                list.add(s);
-            }
+            list.add(s);
             return;
         }
-        solve(s+'0',n,list);
-        solve(s+'1',n,list);
-    }
-    static boolean check(String s){
-        int l=1;
-        // int count1=0;
-        // int count0=0;
-        boolean temp=true;
-        while(l<=s.length()){
-            int count1=0;
-            int count0=0;
-            for(int i=0;i<l;i++){
-                if(s.charAt(i)=='1') count1++;
-                if(s.charAt(i)=='0') count0++;
-            }
-            if(count1<count0) temp=false;
-            l++;
+        if(count0<count1){
+            solve(n,count0+1,count1,s+"0");
+             
         }
-        return temp;
+        solve(n,count0,count1+1,s+"1");
+        
     }
 }
