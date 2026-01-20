dce018fcf31648abd8541742cfc75a405eb61d75
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-01-20 10:31:43 +0530

    Time: 1 ms (100%), Space: 46.8 MB (75.76%) - LeetHub

---
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n=nums.size();
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums.get(i)==2) {
+                ans[i]=-1;
+                continue;
+            }
+            for(int j=0;j<32;j++){
+                if((nums.get(i) & (1<<j))>0){
+                    continue;
+                }
+                int prev=(j-1);
+                //make previous bit 0
+                int x=nums.get(i)^(1<<(prev));
+                ans[i]=x;
+                break;
+            }
+        }
+        return ans;
+    }
+}
