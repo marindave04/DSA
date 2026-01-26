60b5eb3085496caa79572fd195187682c465095a
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-01-26 09:22:35 +0530

    Update Solution.java

--- tree/traversal/0144-binary-tree-preorder-traversal/Solution.java
+++ tree/traversal/0144-binary-tree-preorder-traversal/Solution.java
@@ -1,1 +1,17 @@
-add LC 144 binary tree preorder traversal
+// LeetCode 144: Binary Tree Preorder Traversal
+// Approach:  (Recursive)
+// Time: O(n)
+// Space: O(h)
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        solve(root,list);
+        return list;
+    }
+    static void solve(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        list.add(root.val);
+        solve(root.left,list);
+        solve(root.right,list);
+    }
+}
