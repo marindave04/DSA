c9fd1e3aa4747a4cf48f37e2504f916d4dcd08d5
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-01-28 12:39:28 +0530

    Update Solutions.java

--- tree/BFS/107. Binary Tree Level Order Traversal II/Solutions.java
+++ tree/BFS/107. Binary Tree Level Order Traversal II/Solutions.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+          List<List<Integer>> result = new ArrayList<>();
+        if(root==null) return result;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int lsize=q.size();
+            List<Integer> list=new ArrayList<>();
+           for(int i=0;i<lsize;i++){
+            TreeNode curr=q.poll();
+            list.add(curr.val);
+            if(curr.left!=null) q.offer(curr.left);
+            if(curr.right!=null) q.offer(curr.right);
+           }
+           result.add(0,list);
+        }
+        return result;
+    }
+}
