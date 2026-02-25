38da346b2f910cf5a0f9eee963ed7d1c077151b4
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-02-25 06:46:13 -0800

    Update Solutions.java

--- Array/SlidingWindow/Longest Span in two Binary Arrays/Solutions.java
+++ Array/SlidingWindow/Longest Span in two Binary Arrays/Solutions.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public int equalSumSpan(int[] a1, int[] a2) {
+        // code here
+        int n=a1.length;
+        int p1=0;
+        int p2=0;
+        int max=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            p1+=a1[i];
+            p2+=a2[i];
+            int diff=p1-p2;
+            if(diff==0){
+                max=Math.max(max,i+1);
+            }else if(map.containsKey(diff)){
+                max=Math.max(max,i-map.get(diff));
+            }else{
+                map.put(diff,i);
+            }
+           
+        }
+        return max;
+    }
+}
