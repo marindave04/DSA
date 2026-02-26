fce06dda57f9152db9d4ef1e41c15998379ad1df
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-02-25 23:43:11 -0800

    Update Solutions.java

--- LinkedList/1019. Next Greater Node In Linked List/Solutions.java
+++ LinkedList/1019. Next Greater Node In Linked List/Solutions.java
@@ -29,3 +29,34 @@ class Solution {
       return ans;
     }
 }
+//Approach 2: Monotonic Stack, 1 Pass
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+      ArrayList<Integer> list=new ArrayList<>();
+       Stack<int[]> st=new Stack<>();
+       int idx=0;
+       while(head!=null){
+        list.add(0);
+        while(!st.isEmpty() && head.val>st.peek()[1]){
+            int[] curr=st.peek();
+            int id=curr[0];
+            int val=curr[1];
+            st.pop();
+            list.set(id,head.val);
+        }
+        st.add(new int[]{idx++,head.val});
+        head=head.next;
+       }
+       return list.stream().mapToInt(i->i).toArray();
+    }
+}
