0a15d696e4d7b5742358705e5acc087c068ee3a1
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-02 02:46:09 -0800

    Update Solutions.java

--- tree/DFS/129. Sum Root to Leaf Numbers/Solutions.java
+++ tree/DFS/129. Sum Root to Leaf Numbers/Solutions.java
@@ -35,3 +35,44 @@ class Solution {
         }
     }
 }
+//WITHOUT USING GLOBAL VARIABLE
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  
+    public int sumNumbers(TreeNode root) {
+       
+        int x=0;
+       return solve(root,x);
+        
+    }
+    static int solve(TreeNode root,int x){
+        if(root.left==null && root.right==null){
+            x=root.val+x*10;
+          
+            return x;
+        }
+        int left=0;
+        int right=0;
+        if(root.left!=null){
+          left =  solve(root.left,root.val+x*10);
+        }
+        if(root.right!=null){
+          right =  solve(root.right,root.val+x*10);
+        }
+        return left+right;
+    }
+}
