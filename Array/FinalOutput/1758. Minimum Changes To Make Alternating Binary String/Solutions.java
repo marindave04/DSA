bdbc4b80f01f396cb02af52b9b2952ab55ba778a
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-04 21:12:19 -0800

    Update Solutions.java

--- Array/FinalOutput/1758. Minimum Changes To Make Alternating Binary String/Solutions.java
+++ Array/FinalOutput/1758. Minimum Changes To Make Alternating Binary String/Solutions.java
@@ -1,3 +1,4 @@
+//Approach 1: Start with Zero or Start with One
 class Solution {
     public int minOperations(String s) {
       int s0=0;
@@ -18,5 +19,22 @@ class Solution {
         }
       }
       return Math.min(s0,s1);
+    }
+}
+//Approach 2: Only Check One
+class Solution {
+    public int minOperations(String s) {
+      int s0=0;
+      
+      for(int i=0;i<s.length();i++){
+        if(i%2==0){
+            if(s.charAt(i)!='0') s0++;
+        }else{
+            if(s.charAt(i)!='1') s0++;
+        }
+          
+      }
+      
+      return Math.min(s0,s.length()-s0);
     }
 }
