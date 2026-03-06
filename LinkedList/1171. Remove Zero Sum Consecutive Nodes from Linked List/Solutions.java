40384a607e6ad4720e534717a951076ad2844eea
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-06 03:55:44 -0800

    Update Solutions.java

--- LinkedList/1171. Remove Zero Sum Consecutive Nodes from Linked List/Solutions.java
+++ LinkedList/1171. Remove Zero Sum Consecutive Nodes from Linked List/Solutions.java
@@ -29,3 +29,36 @@ class Solution {
         return dummy.next;
     }
 }
+//Approach 2: Prefix Sum Hash Table
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode dummy=new ListNode(0,head);
+        HashMap<Integer,ListNode> map=new HashMap<>();
+        map.put(0,dummy);
+        int psum=0;
+        ListNode current=dummy;
+        while(current!=null){
+            psum+=current.val;
+            map.put(psum,current);
+            current=current.next;
+        }
+        psum=0;
+        current=dummy;
+        while(current!=null){
+            psum+=current.val;
+            current.next=map.get(psum).next;
+            current=current.next;
+        }
+        return dummy.next;
+    }
+}
