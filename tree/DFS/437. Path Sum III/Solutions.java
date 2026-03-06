2679f6362ce95adebb98dd709087655034459f0d
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-06 00:17:16 -0800

    Update Solutions.java

--- tree/DFS/437. Path Sum III/Solutions.java
+++ tree/DFS/437. Path Sum III/Solutions.java
@@ -39,3 +39,44 @@ class Solution {
         solve(root.right,target,sum);
     }
 }
+//optimized approach using hashmap and prefix sum
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   private int total=0;
+    public int pathSum(TreeNode root, int target) {
+        HashMap<Long,Integer> map=new HashMap<>();
+        long psum=0;
+        map.put(psum,1);
+        solve(root,target,psum,map);
+        return total;
+    }
+    private void solve(TreeNode root,int target,long psum,  HashMap<Long,Integer> map){
+       if(root==null){
+     
+       return;
+       } 
+       psum=(long)psum+root.val;
+      
+       if(map.containsKey(psum-target)){
+        total=total+map.get(psum-target);
+       }
+       map.put(psum,map.getOrDefault(psum,0)+1);
+       solve(root.left,target,psum,map);
+       solve(root.right,target,psum,map);
+       map.put(psum, map.get(psum) - 1);
+    }
+}
