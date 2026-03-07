d20dfa34a4ad8a43b5a05a531ecd5ecd6fcc8ef3
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-07 03:47:12 -0800

    Update Solutions.java

--- Array/SlidingWindow/1888. Minimum Number of Flips to Make the Binary String Alternating/Solutions.java
+++ Array/SlidingWindow/1888. Minimum Number of Flips to Make the Binary String Alternating/Solutions.java
@@ -1,3 +1,5 @@
+//APPROACH-1
+
 class Solution {
     public int minFlips(String s) {
        int n=s.length();
@@ -32,6 +34,47 @@ class Solution {
             res=Math.min(res,Math.min(f1,f2));
             if(s.charAt(i)!=s1.charAt(i)) f1--;
              if(s.charAt(i)!=s2.charAt(i)) f2--;
+            i++;
+          }
+       }
+       return res;
+    }
+}
+//APPROACH-2
+class Solution {
+    public int minFlips(String s) {
+       int n=s.length();
+      
+       //generating final string
+         StringBuilder s1=new StringBuilder();
+         StringBuilder s2=new StringBuilder();
+
+      for(int i=0;i<2*n;i++){
+        if(i%2==0){
+           s1.append('0');
+           s2.append('1');
+         }else{
+        s1.append('1');
+        s2.append('0');
+       }
+      }
+
+    String str1=s1.toString();
+    String str2=s2.toString();
+       int i=0;
+       int j=0;
+       int res=Integer.MAX_VALUE;
+       int f1=0;
+       int f2=0;
+       while(j<2*n){
+          if((j-i+1)<=n){
+            if(s.charAt(j%n)!=s1.charAt(j)) f1++;
+            if(s.charAt(j%n)!=s2.charAt(j)) f2++;
+            j++;
+          }else{
+            res=Math.min(res,Math.min(f1,f2));
+            if(s.charAt(i%n)!=s1.charAt(i)) f1--;
+             if(s.charAt(i%n)!=s2.charAt(i)) f2--;
             i++;
           }
        }
