bbc6a5f32f7a0584eed719a741582f0573222d72
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-07 21:10:55 -0800

    Create Solutions.java

---
+++ Array/Simulation/3861. Minimum Capacity Box/Solutions.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int min=Integer.MAX_VALUE;
+        int ans=-1;
+        for(int i=0;i<capacity.length;i++){
+            if(capacity[i]>=itemSize){
+                if(capacity[i]<min){
+                    ans=i;
+                    min=capacity[i];
+                }
+            }
+        }
+       
+        return ans;
+    }
+}
