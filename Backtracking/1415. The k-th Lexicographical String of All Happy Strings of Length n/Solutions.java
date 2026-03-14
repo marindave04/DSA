95becee9ac37a24adf8958c6bb66ca78c0bba61e
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-14 05:27:35 -0700

    Update Solutions.java

--- Backtracking/1415. The k-th Lexicographical String of All Happy Strings of Length n/Solutions.java
+++ Backtracking/1415. The k-th Lexicographical String of All Happy Strings of Length n/Solutions.java
@@ -30,3 +30,41 @@ class Solution {
        
     } 
 }
+/*
+| Approach                                                                       | Time Complexity          | Space Complexity |
+| ------------------------------------------------------------------------------ | ------------------------ | ---------------- |
+| **Backtracking with Early Stopping (Generate Happy Strings until k-th found)** | **O(2ⁿ)** *(worst case)* | **O(n)**         |
+
+*/
+class Solution {
+
+    public String getHappyString(int n, int k) {
+        
+       char[] a=new char[3];
+       String[] ans=new String[1];
+       int[] count=new int[1];
+       a[0]='a';
+       a[1]='b';
+       a[2]='c';
+     
+        solve(new StringBuilder(),a,n,ans,count,k);
+       if(ans[0]==null) return "";
+       return ans[0];
+    }
+    static void solve(StringBuilder sb,char[] a,int n,String[] ans,int[] count,int k){
+        if(sb.length()==n){
+           count[0]++;
+           if(count[0]==k){
+            ans[0]=sb.toString();
+           }
+           return;
+        }
+        for(int i=0;i<3;i++){
+            if(sb.length()>0 && sb.charAt(sb.length()-1)==a[i]) continue;
+             solve(sb.append(a[i]),a,n,ans,count,k);
+             if(ans[0]!=null) return;
+             sb.deleteCharAt(sb.length() - 1);
+        }
+       
+    } 
+}
