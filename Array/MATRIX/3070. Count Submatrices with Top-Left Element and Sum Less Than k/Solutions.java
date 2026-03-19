cbda149da9752a574cc2672cfb0b928de0685fd4
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-03-18 21:43:48 -0700

    Update Solutions.java

--- Array/MATRIX/3070. Count Submatrices with Top-Left Element and Sum Less Than k/Solutions.java
+++ Array/MATRIX/3070. Count Submatrices with Top-Left Element and Sum Less Than k/Solutions.java
@@ -25,3 +25,59 @@ class Solution {
         return count;
     }
 }
+/*
+| Approach                                                                       | Time Complexity | Space Complexity |
+| ------------------------------------------------------------------------------ | --------------- | ---------------- |
+| **2D Prefix Sum (Row-wise + Accumulated column sum for submatrix from (0,0))** | **O(m · n)**    | **O(m · n)**     |
+
+*/
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int count=0;
+        int[][] psum1=new int[m][n];
+         int[][] psum2=new int[m][n];
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum=grid[i][j]+(j>0 ? psum1[i][j-1] : 0);
+                psum1[i][j]=sum;
+                if(i>0){
+                    sum+=psum2[i-1][j];
+                }
+                psum2[i][j]=sum;
+                if(sum<=k) count++;
+            }
+        }
+        return count;
+    }
+}
+/*
+| Approach                                                                              | Time Complexity | Space Complexity |
+| ------------------------------------------------------------------------------------- | --------------- | ---------------- |
+| **Optimized 2D Prefix Sum (In-place row prefix + 1D buffer for column accumulation)** | **O(m · n)**    | **O(n)**         |
+
+*/
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int count=0;
+        int[][] psum1=new int[m][n];
+         int[][] psum2=new int[m][n];
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum=grid[i][j]+(j>0 ? psum1[i][j-1] : 0);
+                psum1[i][j]=sum;
+                if(i>0){
+                    sum+=psum2[i-1][j];
+                }
+                psum2[i][j]=sum;
+                if(sum<=k) count++;
+            }
+        }
+        return count;
+    }
+}
