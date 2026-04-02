c91dc251e694ceac90586e263259ec6d73aeb712
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-02 00:15:22 -0700

    Update Solutions.java

--- tree/DFS/2458. Height of Binary Tree After Subtree Removal Queries/Solutions.java
+++ tree/DFS/2458. Height of Binary Tree After Subtree Removal Queries/Solutions.java
@@ -89,3 +89,64 @@ class Solution {
         rightToLeft(root.left,dis+1);
     }
 }
+/*
+| Approach                                            | Time Complexity | Space Complexity |
+| --------------------------------------------------- | --------------- | ---------------- |
+| **Single DFS + Level-wise Top 2 Heights (Optimal)** | **O(n + q)**    | **O(n)**         |
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //storing height of each node
+    private int[] heights=new int[100001];
+    //storing level of each node
+    private int[] levels=new int[100001];
+    //storing max of each level
+    private int[] levelMax=new int[100001];
+    //storing 2nd max of each level
+    private int[] levelSecondMax=new int[100001];
+    public int[] treeQueries(TreeNode root, int[] queries) {
+       //populating each declared data structures
+       
+       solve(root,0);
+       int m=queries.length;
+       int[] ans=new int[m];
+       for(int i=0;i<m;i++){
+         int node = queries[i];
+            int h = heights[node];
+            int l = levels[node];
+
+            int best = (h == levelMax[l]) ? levelSecondMax[l] : levelMax[l];
+            ans[i] = l + best - 1;
+       }
+       return ans;
+    }
+    private int solve(TreeNode root,int level){
+        if(root==null) return 0;
+        levels[root.val]=level;
+        int l=solve(root.left,level+1);
+        int r=solve(root.right,level+1);
+        heights[root.val]=Math.max(l,r)+1;
+        if(levelMax[level]< heights[root.val]){
+            levelSecondMax[level]=levelMax[level];
+            levelMax[level]=heights[root.val];
+        }else if(levelSecondMax[level]<heights[root.val]){
+             levelSecondMax[level]=heights[root.val];
+        }
+        return  heights[root.val];
+    }
+}
