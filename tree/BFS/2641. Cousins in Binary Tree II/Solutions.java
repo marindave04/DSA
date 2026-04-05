2759ba3f853854281641bd87896ad39842301e83
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-04 23:17:24 -0700

    Update Solutions.java

--- tree/BFS/2641. Cousins in Binary Tree II/Solutions.java
+++ tree/BFS/2641. Cousins in Binary Tree II/Solutions.java
@@ -69,3 +69,65 @@ class Solution {
        }
     }
 }
+/*
+| Approach                                   | Time Complexity | Space Complexity |
+| ------------------------------------------ | --------------- | ---------------- |
+| **Level Order BFS (Compute Sum + Update)** | **O(n)**        | **O(n)**         |
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> q1=new LinkedList<>();
+        Queue<TreeNode> q2=new LinkedList<>();
+        q1.offer(root);
+        while(!q1.isEmpty()){
+            int lsize=q1.size();
+            for(int i=0;i<lsize;i++){
+                TreeNode curr=q1.poll();
+                if(curr.left!=null){
+                    q2.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q2.add(curr.right);
+                }
+                q1.offer(curr);
+            }
+            int sum=0;
+            int lsize2=q2.size();
+            for(int i=0;i<lsize2;i++){
+                TreeNode curr=q2.poll();
+                sum+=curr.val;
+            }
+            int lsize3=q1.size();
+            for(int i=0;i<lsize3;i++){
+                TreeNode curr=q1.poll();
+                int siblingSum=(curr.left!=null ? curr.left.val : 0) + (curr.right!=null ? curr.right.val : 0);
+                if(curr.left!=null){
+                    curr.left.val=sum- siblingSum;
+                    q1.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    curr.right.val=sum- siblingSum;
+                    q1.offer(curr.right);
+                }
+            }
+        }
+        root.val=0;
+        return root;
+    }
+}
