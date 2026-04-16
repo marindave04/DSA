f188683dda8d7be60ef09f266c90ce7ce27b3dfc
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-15 19:00:44 -0700

    Create Solutions.java

---
+++ tree/DFS/2196. Create Binary Tree From Descriptions/Solutions.java
@@ -0,0 +1,6 @@
+/*
+| Approach                                                  | Time Complexity | Space Complexity |
+| --------------------------------------------------------- | --------------- | ---------------- |
+| **Hashing + DFS (Build Tree using Parent-Child Mapping)** | **O(n)**        | **O(n)**         |
+
+*/
