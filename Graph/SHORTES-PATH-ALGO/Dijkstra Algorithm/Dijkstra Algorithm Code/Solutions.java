1d6e7f3e78a23d84b7eaefdbfd2922c6e3a0cadb
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-17 21:52:24 -0700

    Update Solutions.java

--- Graph/SHORTES-PATH-ALGO/Dijkstra Algorithm/Dijkstra Algorithm Code/Solutions.java
+++ Graph/SHORTES-PATH-ALGO/Dijkstra Algorithm/Dijkstra Algorithm Code/Solutions.java
@@ -41,3 +41,51 @@ class Solution {
         return result;
     }
 }
+/*
+| Approach                                          | Time Complexity      | Space Complexity |
+| ------------------------------------------------- | -------------------- | ---------------- |
+| **Dijkstra’s Algorithm (TreeSet as Ordered Set)** | **O((V + E) log V)** | **O(V + E)**     |
+
+*/
+class Solution {
+    public int[] dijkstra(int V, int[][] edges, int src) {
+        // code here
+        //Make ADJENCY LIST
+        List<List<int[]>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] row:edges){
+            int u=row[0];
+            int v=row[1];
+            int w=row[2];
+            adj.get(u).add(new int[]{v,w});
+            adj.get(v).add(new int[]{u,w});
+        }
+        int[] result=new int[V];
+        Arrays.fill(result,Integer.MAX_VALUE);
+        result[src]=0;
+        TreeSet<int[]> st=new TreeSet<>((a,b)->{
+            if(a[0]==b[0]) return a[1]-b[1];
+            return a[0]-b[0];
+        });
+        st.add(new int[]{0,src});
+        while(!st.isEmpty()){
+            int curr[]=st.pollFirst();
+            int dist=curr[0];
+            int u=curr[1];
+            for(int[] neigh:adj.get(u)){
+                int v=neigh[0];
+                int w=neigh[1];
+                if(dist+w<result[v]){
+                    if(result[v]!=Integer.MAX_VALUE){
+                        st.remove(new int[]{result[v],v});
+                    }
+                    result[v]=dist+w;
+                    st.add(new int[]{result[v],v});
+                }
+            }
+        }
+        return result;
+    }
+}
