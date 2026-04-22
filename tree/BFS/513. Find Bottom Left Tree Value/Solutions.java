f608f5536a6b14b11e2a23a7b455e1555b175b4d
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-21 20:50:10 -0700

    Create Solutions.java

---
+++ tree/BFS/513. Find Bottom Left Tree Value/Solutions.java
@@ -0,0 +1,46 @@
+/*
+| Approach                                                                                           | Time Complexity | Space Complexity |
+| -------------------------------------------------------------------------------------------------- | --------------- | ---------------- |
+| **Level order traversal (BFS), take first node of each level → last level’s first node is answer** | **O(n)**        | **O(n)**         |
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        int ans=-1;
+        while(!q.isEmpty()){
+            int size=q.size();
+            boolean top=false;
+            for(int i=0;i<size;i++){
+               TreeNode curr=q.poll();
+               if(!top){
+                ans=curr.val;
+                top=true;
+               }
+               if(curr.left!=null){
+                q.offer(curr.left);
+               }
+               if(curr.right!=null){
+                q.offer(curr.right);
+               }
+            }
+        }
+        return ans;
+    }
+}
