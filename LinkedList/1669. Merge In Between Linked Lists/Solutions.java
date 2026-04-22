dd9a81660c64a2beefbab089cd2e2efed386f745
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-04-21 21:55:49 -0700

    Update Solutions.java

--- LinkedList/1669. Merge In Between Linked Lists/Solutions.java
+++ LinkedList/1669. Merge In Between Linked Lists/Solutions.java
@@ -45,3 +45,29 @@ class Solution {
         return list1;
     }
 }
+/*
+| Approach                                                                                        | Time Complexity | Space Complexity |
+| ----------------------------------------------------------------------------------------------- | --------------- | ---------------- |
+| Find tail of list2, move pointer1 to (a-1) and pointer2 to (b+1), then connect list2 in between | **O(n + m)**    | **O(1)**         |
+
+*/
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode e=list2;
+        ListNode s=list2;
+        while(e.next!=null){
+            e=e.next;
+        }
+        ListNode pointer1=list1;
+        for(int i=0;i<a-1;i++){
+            pointer1=pointer1.next;
+        }
+         ListNode pointer2=list1;
+        for(int i=0;i<b+1;i++){
+            pointer2=pointer2.next;
+        }
+        pointer1.next=s;
+        e.next=pointer2;
+        return list1;
+    }
+}
