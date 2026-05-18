ad038ab1688c7ced19447e95f5c8bf0cbbcaffd4
marindave04 <mdsadainsiddiqui973@gmail.com>
2026-05-18 10:33:41 -0700

    Update Solutions.java

--- Array/BinarySearch/153. Find Minimum in Rotated Sorted Array/Solutions.java
+++ Array/BinarySearch/153. Find Minimum in Rotated Sorted Array/Solutions.java
@@ -27,3 +27,29 @@ class Solution {
         return 0;
     }
 }
+/*| Approach                                                                                                                     | Time         | Space    |
+| ---------------------------------------------------------------------------------------------------------------------------- | ------------ | -------- |
+| Use binary search to locate the rotation pivot/valley point by comparing middle element with neighbors and array end values. | **O(log N)** | **O(1)** |
+*/
+class Solution {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        //first we will find valley point;
+        int min=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid-1>=0 && mid+1<n && nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
+                return nums[mid];
+            }else if(mid+1< n && nums[mid]>nums[mid+1]){
+               return nums[mid+1];
+            }else if(nums[mid]<=nums[n-1]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return nums[0];
+    }
+}
